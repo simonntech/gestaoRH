84a70735ea72e14bd91fde7fe598c1bed60c95cd
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-11-08 16:12:15 -0300

    getEmployees Service

---
+++ src/services/employees-services.ts
@@ -0,0 +1,16 @@
+import { EmployeesModel } from "../models/employees-model";
+import * as EmployeesRepository from "../repositories/employees-repository";
+import * as HttpResponse from "../utils/http-helper";
+
+export const getEmployeesService = async () => {
+    const data = await EmployeesRepository.listAllEmployees();
+    let response = null;
+
+    if (data) {
+        response = await HttpResponse.ok(data);
+    } else {
+        response = await HttpResponse.noContent();
+
+        return response;
+    }
+}
